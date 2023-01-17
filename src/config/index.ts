66f012a453c207f74095222a2b26b4ebc948dc53
Xiao Sa <sa.xiao@thoughtworks.com>
2023-01-17 16:28:44 +0800

    <fix, xs, [E-14]>: support https

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
-export const FRONTEND_URL = "http://www.xiaostudy.cn";
+export const FRONTEND_URL = "https://www.xiaostudy.cn";
 
-export const BACKEND_URL = "http://www.xiaostudy.cn:3000/api";
+export const BACKEND_URL = "https://www.xiaostudy.cn/api";
 
 export const GITHUB_LOGIN_URL = `https://github.com/login/oauth/authorize?client_id=bad5d978cbbb678fb88c&redirect_uri=${FRONTEND_URL}/login`;
